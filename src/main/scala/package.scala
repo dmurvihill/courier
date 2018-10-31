eda2748a48eb13a742270b241dc9e1806932e8e0
vaas <auspex@auspex.eu>
2018-10-31 11:24:25 +0800

    Fixed implicit conversion of email addresses

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -1,12 +1,13 @@
 import javax.mail.internet.InternetAddress
+import scala.language.implicitConversions
 
 /** An agreeable email interface for scala. */
 package object courier {
 
+  implicit def str2Address(name: String) = new InternetAddress(name)
+
   implicit class addr(name: String){
     def `@`(domain: String): InternetAddress = new InternetAddress(s"$name@$domain")
     def at = `@` _
-    /** In case whole string is email address already */
-    def addr = new InternetAddress(name)
   }
 }
