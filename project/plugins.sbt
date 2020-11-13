a5fda93b5773724c70bb8cdc3335cf8033633091
David Francoeur <dfrancoeur04@gmail.com>
2020-11-12 22:38:27 -0500

    Cross compile for scala 3.0.0-M1

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,4 @@
 addSbtPlugin("io.crashbox" % "sbt-gpg" % "0.2.0")
 addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "2.3")
 addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.5.1")
-addSbtPlugin("ch.epfl.lamp" % "sbt-dotty" % "0.4.1")
+addSbtPlugin("ch.epfl.lamp" % "sbt-dotty" % "0.4.4")
--- src/test/scala/mailspec.scala
+++ src/test/scala/mailspec.scala
@@ -4,9 +4,6 @@ import java.util.Properties
 
 import javax.mail.Provider
 import org.jvnet.mock_javamail.{Mailbox, MockTransport}
-import org.scalatest._
-import wordspec._
-import matchers._
 
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -14,27 +11,26 @@ import scala.concurrent.duration._
 
 class MockedSMTPProvider extends Provider(Provider.Type.TRANSPORT, "mocked", classOf[MockTransport].getName, "Mock", null)
 
-class MailSpec extends AnyWordSpec with should.Matchers {
+class MailSpec extends munit.FunSuite {
   private val mockedSession = javax.mail.Session.getDefaultInstance(new Properties() {{
     put("mail.transport.protocol.rfc822", "mocked")
   }})
   mockedSession.setProvider(new MockedSMTPProvider)
 
-  "the mailer" should {
-    "send an email" in {
-      val mailer = Mailer(mockedSession)
-      val future = mailer(Envelope.from("someone@example.com".addr)
-            .to("mom@gmail.com".addr)
-            .cc("dad@gmail.com".addr)
-            .subject("miss you")
-            .content(Text("hi mom")))
 
-      Await.ready(future, 5.seconds)
-      val momsInbox = Mailbox.get("mom@gmail.com")
-      momsInbox.size === 1
-      val momsMsg = momsInbox.get(0)
-      momsMsg.getContent === "hi mom"
-      momsMsg.getSubject === "miss you"
-    }
+  test("the mailer should send an email") {
+    val mailer = Mailer(mockedSession)
+    val future = mailer(Envelope.from("someone@example.com".addr)
+          .to("mom@gmail.com".addr)
+          .cc("dad@gmail.com".addr)
+          .subject("miss you")
+          .content(Text("hi mom")))
+
+    Await.ready(future, 5.seconds)
+    val momsInbox = Mailbox.get("mom@gmail.com")
+    assertEquals(momsInbox.size, 1)
+    val momsMsg = momsInbox.get(0)
+    assertEquals(momsMsg.getContent, "hi mom")
+    assertEquals(momsMsg.getSubject, "miss you")
   }
 }
