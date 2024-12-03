6c5b137069c2aceeadca7b5a6ada4a6ecba883bb
David Francoeur <dfrancoeur04@gmail.com>
2024-12-02 23:23:31 -0500

    tests: add a note on how to generate password

--- integration-tests/src/test/scala/mailspec.scala
+++ integration-tests/src/test/scala/mailspec.scala
@@ -6,6 +6,7 @@ import scala.concurrent.duration._
 
 
 class MailSpec extends munit.FunSuite {
+  // create a gmail app password https://myaccount.google.com/apppasswords
   test("the mailer should send an email") {
     assume(sys.env("CI").isEmpty(), "This test is meant to be ran locally.")
     val email = sys.env("IT_EMAIL")
