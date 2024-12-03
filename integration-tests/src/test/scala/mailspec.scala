4243f047e6a03f41ab2a34a29c4637d2d0490b53
David Francoeur <dfrancoeur04@gmail.com>
2024-12-02 23:16:00 -0500

    tests: add integration test

---
+++ integration-tests/src/test/scala/mailspec.scala
@@ -0,0 +1,23 @@
+package courier
+
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+
+
+class MailSpec extends munit.FunSuite {
+  test("the mailer should send an email") {
+    assume(sys.env("CI").isEmpty(), "This test is meant to be ran locally.")
+    val email = sys.env("IT_EMAIL")
+    val password = sys.env("IT_PASSWORD")
+    val mailer = Mailer("smtp.gmail.com", 587)
+                .auth(true)
+                .as(email, password)
+                .startTls(true)()
+    val mId = Await.result(mailer(Envelope.from(email.addr)
+            .to(email.addr)
+            .subject("miss you")
+            .content(Text("hi mom"))), 10.seconds)
+    assert(mId.nonEmpty, "Message-ID should be set by the Transport.send call")
+  }
+}
